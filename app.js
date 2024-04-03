a172657c522e6b3fa6549a2a33503c91c0c28f89
unknown <abdullahabirweb10@gmail.com>
2024-04-03 22:01:11 +0600

    variable

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-WHY USE STRIC ?
+// WHY USE STRIC ?
 
- USE STRIC ALWAYS TOP!
-"use stric";
-console.log("HELLO");
-alert("HELLO")
-
-
-
-Variable 
-
-const person = {
-  name: "abir",
-  age: 20,
-  Number: "0020022",
-};
-
-let name;     ----- declare
-name = "abir";       ----- define 
-
-alert(name)
-console.log(name);
-
-let name = "abir";  ------ei variable ta tokhon use korbo jkhn amra jani ei variable e value tai use hobe!
-console.log(name); 
+//  USE STRIC ALWAYS TOP!
+// "use stric";
+// console.log("HELLO");
+// alert("HELLO")
 
 
-let name = "abir";
-let age = 18;
 
-let name = "abir", age = "18";
+// Variable 
+
+// let name;     ----- declare
+// name = "abir";       ----- define 
+
+// alert(name)
+// console.log(name);
+
+// let name = "abir";  //------ei variable ta tokhon use korbo jkhn amra jani ei variable e value tai use hobe!
+// console.log(name); 
+
+
+// let name = "abir";
+// let age = 18;
+
+// let name = "abir", age = "18";
+
+// let name = "abir";
+// let age = "20";
+
+// alert(age);
+// alert (name);
+
+// Variable End //
+
+
+
